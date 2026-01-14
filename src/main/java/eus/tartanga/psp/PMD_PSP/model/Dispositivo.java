46438e65b2b767b0a55b8963ba5a1a81dd1d4423
Jago128 <jagobaetxegarai@gmail.com>
2026-01-14 13:25:05 +0100

    Formattiiiiiiiiiiiiiing

--- src/main/java/eus/tartanga/psp/PMD_PSP/model/Dispositivo.java
+++ src/main/java/eus/tartanga/psp/PMD_PSP/model/Dispositivo.java
@@ -1,6 +1,5 @@
 package eus.tartanga.psp.PMD_PSP.model;
 
 public enum Dispositivo {
-
 	TABLET, MOVIL;
 }
--- src/main/java/eus/tartanga/psp/PMD_PSP/service/Service.java
+++ src/main/java/eus/tartanga/psp/PMD_PSP/service/Service.java
@@ -1,5 +1,11 @@
 package eus.tartanga.psp.PMD_PSP.service;
 
+import java.util.ArrayList;
+
+import eus.tartanga.psp.PMD_PSP.model.Game;
+
 public class Service {
 
+	private ArrayList<Game> games;
+	
 }
