e2abd75ad9efa3394180aa2783a9f62ac59c11b5
Jago128 <jagobaetxegarai@gmail.com>
2026-01-14 12:43:24 +0100

    Added classes

--- src/main/java/eus/tartanga/psp/PMD_PSP/Main.java
+++ src/main/java/eus/tartanga/psp/PMD_PSP/Main.java
@@ -4,7 +4,7 @@ package eus.tartanga.psp.PMD_PSP;
  * Hello world!
  *
  */
-public class App
+public class Main
 {
     public static void main( String[] args )
     {
---
+++ src/main/java/eus/tartanga/psp/PMD_PSP/model/Game.java
@@ -0,0 +1,5 @@
+package eus.tartanga.psp.PMD_PSP.model;
+
+public class Game {
+
+}
