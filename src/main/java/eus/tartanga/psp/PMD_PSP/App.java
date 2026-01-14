f54ed867fa9a05f67d69c5c985d9ea85f56bcd3a
Jago128 <jagobaetxegarai@gmail.com>
2026-01-14 12:24:31 +0100

    Create repository

---
+++ src/main/java/eus/tartanga/psp/PMD_PSP/App.java
@@ -0,0 +1,13 @@
+package eus.tartanga.psp.PMD_PSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
